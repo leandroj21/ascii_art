f504d2606019cd137be9185fe80a607c21e84d5a
Leandro Jiménez <leandroj21p@gmail.com>
2023-08-08 22:15:23 -0400

    feat: print ascii art with colors

--- src/ImageASCIIArt.py
+++ src/ImageASCIIArt.py
@@ -1,5 +1,25 @@
 from PIL import Image
+from colorama import Fore, Style
 
+# [COLOR, INITIAL RGB RANGE, FINAL RGB RANGE]
+_colors_comparisons = [
+    [Fore.BLACK, (0, 0, 0), (85, 85, 85)],
+    [Fore.RED, (85, 0, 0), (170, 85, 85)],
+    [Fore.GREEN, (0, 85, 0), (85, 170, 85)],
+    [Fore.YELLOW, (85, 85, 0), (170, 170, 85)],
+    [Fore.BLUE, (0, 0, 85), (85, 85, 170)],
+    [Fore.MAGENTA, (85, 0, 85), (170, 85, 170)],
+    [Fore.CYAN, (0, 85, 85), (85, 170, 170)],
+    [Fore.WHITE, (85, 85, 85), (170, 170, 170)],
+    [Fore.LIGHTBLACK_EX, (170, 170, 170), (191, 191, 191)],
+    [Fore.LIGHTRED_EX, (170, 0, 0), (255, 85, 85)],
+    [Fore.LIGHTGREEN_EX, (0, 170, 0), (85, 255, 85)],
+    [Fore.LIGHTYELLOW_EX, (170, 170, 0), (255, 255, 85)],
+    [Fore.LIGHTBLUE_EX, (0, 0, 170), (85, 85, 255)],
+    [Fore.LIGHTMAGENTA_EX, (170, 0, 170), (255, 85, 255)],
+    [Fore.LIGHTCYAN_EX, (0, 170, 170), (85, 255, 255)],
+    [Fore.LIGHTWHITE_EX, (170, 170, 170), (255, 255, 255)]
+]
 
 class ImageASCIIArt:
     """
@@ -50,7 +70,7 @@ class ImageASCIIArt:
 
         self._load_image()
 
-    def _load_image(self):
+    def _load_image(self) -> None:
         """Loads the image into the class ImageASCIIArt"""
 
         try:
@@ -63,7 +83,42 @@ class ImageASCIIArt:
         except:
             raise Exception("Cannot read the image")
 
-    def print_ascii_art(self, resolution_multiplier=3, fix_resolution=4, invert_brightness=False):
+    def _is_rgb_within_range(self, rgb:tuple, lower_bound:tuple, upper_bound:tuple) -> bool:
+        """Select the color that could represent a pixel
+
+        Parameters
+        ----------
+        rgb : tuple
+            RGB tuple that we need to know if it is between lower_bound and upper_bound
+        lower_bound : tuple
+            RGB tuple that is the lower bound in the comparison
+        upper_bound : tuple
+            RGB tuple that is the upper bound in the comparison
+        """
+        r, g, b = rgb
+        lower_r, lower_g, lower_b = lower_bound
+        upper_r, upper_g, upper_b = upper_bound
+        
+        within_r = lower_r <= r <= upper_r
+        within_g = lower_g <= g <= upper_g
+        within_b = lower_b <= b <= upper_b
+        
+        return within_r and within_g and within_b
+
+    def _select_printing_color(self, pixel:tuple) -> str:
+        """Select the color that could represent a pixel
+
+        Parameters
+        ----------
+        pixel : tuple
+            Pixel in RGB that will be represented with a color from colorama color pallete
+        """
+        for color_comparison in _colors_comparisons:
+            if self._is_rgb_within_range(pixel, color_comparison[1], color_comparison[2]):
+                return color_comparison[0]
+        return Fore.WHITE
+
+    def print_ascii_art(self, resolution_multiplier=3, fix_resolution=4, invert_brightness=False, colored=False) -> None:
         """Print the image using ASCII characters
 
         Parameters
@@ -85,5 +140,8 @@ class ImageASCIIArt:
                 else:
                     index_mapping = (pixel_grayscale_value // fix_resolution) % len(self.mapping_string)
                 
-                print(self.mapping_string[index_mapping] * resolution_multiplier, end="")
+                if colored:
+                    print(f"{self._select_printing_color(self.pixels[i][j])}{self.mapping_string[index_mapping] * resolution_multiplier}{Style.RESET_ALL}", end="")
+                else:
+                    print(self.mapping_string[index_mapping] * resolution_multiplier, end="")
             print("\n", end="")
